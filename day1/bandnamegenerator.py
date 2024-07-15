5d9d076517cb52881d286e9c0c2aaa4baac13b65
lari <popoxta@gmail.com>
2024-07-15 16:30:02 +1200

    Create band name generator

---
+++ day1/bandnamegenerator.py
@@ -0,0 +1,9 @@
+def main():
+    print('Howdy partner!\nWe\'re gonna generate you a smashing band name.')
+    city_name = input('Now, where\'d ya grow up?\n')
+    pet_name = input('And what\'s ya first pet\'s name?\n')
+
+    print(f'Yall\'s band name shall be... The {city_name.title()} {pet_name.title()}s!!!')
+
+
+main()
