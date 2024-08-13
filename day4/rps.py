1e0650f43579a12a76b65977313d7f0fbcf1194e
lari <popoxta@gmail.com>
2024-08-13 16:54:37 +1200

    Formatting + tidy up

--- day4/rps.py
+++ day4/rps.py
@@ -1,4 +1,5 @@
 import random
+
 from prompt import prompt
 
 OPPONENT_NAME = 'COMPUTER'
@@ -6,7 +7,7 @@ CHOICES = ['rock', 'paper', 'scissors']
 
 
 def get_random_choice():
-    return CHOICES[random.randint(0, 2)]
+    return random.choice(CHOICES)
 
 
 def get_user_choice():
@@ -16,16 +17,19 @@ def get_user_choice():
 def is_player_win(player_choice, computer_choice):
     if player_choice == computer_choice:
         return 'draw'
-    elif ((player_choice == 'rock' and computer_choice == 'scissors')
-          or (player_choice == 'scissors' and computer_choice == 'paper')
-          or (player_choice == 'paper' and computer_choice == 'rock')):
+
+    if ((player_choice == 'rock' and computer_choice == 'scissors')
+            or (player_choice == 'scissors' and computer_choice == 'paper')
+            or (player_choice == 'paper' and computer_choice == 'rock')):
         return 'pwin'
+
     return 'cwin'
 
 
 def play_round():
     player_choice = get_user_choice()
     computer_choice = get_random_choice()
+
     print(f'{OPPONENT_NAME} chose {computer_choice}!')
     return is_player_win(player_choice, computer_choice)
 
@@ -37,29 +41,28 @@ def play_game(player_name):
     print(f'\nWelcome to ROCK PAPER SCISSORS {player_name}!\nYour opponent is {OPPONENT_NAME}.')
     print('The game starts now!\n')
 
-    def print_score():
-        print(f'COMPUTER: {computer_score}, PLAYER: {player_score}')
-
     while player_score < 3 and computer_score < 3:
         round_result = play_round()
-        if round_result == 'pwin':
-            print(f'Congrats, {player_name} won!\n')
-            player_score += 1
-        elif round_result == 'cwin':
-            print(f'{OPPONENT_NAME} won!\n')
-            computer_score += 1
-        else:
-            print('It\'s a draw!\n')
 
-        print_score()
+        match round_result:
+            case 'pwin':
+                print(f'Congrats, {player_name} won!\n')
+                player_score += 1
+            case 'cwin':
+                print(f'{OPPONENT_NAME} won!\n')
+                computer_score += 1
+            case _:
+                print('It\'s a draw!\n')
+
+        print(f'COMPUTER: {computer_score}, PLAYER: {player_score}')
 
     winner = player_name if player_score > computer_score else OPPONENT_NAME
     print('\nTHE GAME IS NOW OFFICIALLY OVER!')
     print(f'The winner is...\n{winner}!!!!\n')
 
-    play_again = prompt(name='play_again', message='Play again?', choices=['yes', 'no']) == 'yes'
-    if play_again:
+    if prompt(name='play_again', message='Play again?', choices=['yes', 'no']) == 'yes':
         play_game(player_name)
+
     print(f'Goodbye {player_name}!')
     return
 
